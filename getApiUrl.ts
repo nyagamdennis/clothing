7a343b2b7e09501dfd67366a20f702a66749cd71
dennismnyaga <dennisnyaga042@gmail.com>
2025-04-30 00:09:08 +0300

    new changes

--- getApiUrl.ts
+++ getApiUrl.ts
@@ -1,6 +1,6 @@
 const getApiUrl = () => {
-    const apiUrl = 'http://127.0.0.1:8000';
-    // const apiUrl = 'https://joyce2.pythonanywhere.com';
+    // const apiUrl = 'http://127.0.0.1:8000';
+    const apiUrl = 'https://joyce2.pythonanywhere.com';
     // const apiUrl = 'https://ester.pythonanywhere.com';
     return apiUrl
   }
